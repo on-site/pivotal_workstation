0534f3437badc6dfdc5935fa4f786b1d95636503
Gregory Keeney <gkeeney@on-site.com>
2013-03-13 12:03:17 -0700

    An initial vagrant box setup for onsite development.

---
+++ recipes/onsite_vagrant_development.rb
@@ -0,0 +1,33 @@
+node[:onsite][:ws_dir] ||= "/Users/#{WS_USER}/ws"
+node[:onsite][:vagrant_box_dir] ||= "/Users/#{WS_USER}/.vagrant.d/boxes"
+
+file "/etc/hosts" do
+  owner "root"
+  group "wheel"
+  contents <<EOTXT
+127.0.0.1	localhost
+255.255.255.255	broadcasthost
+::1             localhost 
+fe80::1%lo0	localhost
+10.10.10.10 vagrant
+10.10.10.10 dev.on-site.com
+10.10.10.10 dev.rentaladdress.com
+10.10.10.10 local.on-site.com
+10.10.10.10 local.rentaladdress.com
+10.10.10.10 awesomeapts.local.rentaladdress.com
+10.10.10.10 awesomeapts.dev.rentaladdress.com
+EOTXT
+  action :create
+end
+
+directory "/Users/#{WS_USER}/ws" do
+  owner WS_USER
+  action :create
+end
+
+execute "Create Vagrant Box" do
+  command "vagrant box add precise64 http://files.vagrantup.com/precise64.box"
+  creates "#{node[:onsite][:vagrant_box_dir]}/precise64"
+  user WS_USER
+  action :run
+end
