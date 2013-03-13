3476a3688e143d53ad440ed215ed4d18f6336d83
Gregory Keeney <gkeeney@on-site.com>
2013-03-13 11:23:04 -0700

    An emacs recipe for OS X

---
+++ recipes/emacs.rb
@@ -0,0 +1,6 @@
+dmg_package "Emacs" do
+  source "http://emacsformacosx.com/emacs-builds/Emacs-24.3-universal-10.6.8.dmg"
+  checksum "92b3a6dd0a32b432f45ea925cfa34834c9ac9f7f0384c38775f6760f1e89365a"
+  action :install
+  owner WS_USER
+end
